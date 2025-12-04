2c5cd306803f3b91d3659afe3051f152ae2bd01d
Praveen Reddy <praveenreddyg5612@gmail.com>
2025-12-04 16:00:48 +0530

    route image

--- middleware/uploadProfile.js
+++ middleware/uploadProfile.js
@@ -1,27 +1,23 @@
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
+const cloudinaryStorage = require("multer-storage-cloudinary");
 
-// Cloudinary config (VERY IMPORTANT)
+// Configure cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Storage settings
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: async (req, file) => {
-    return {
-      folder: "cleanpro",            // ⬅ store inside cleanpro/ folder
-      allowed_formats: ["jpg", "jpeg", "png"],
-      transformation: [{ width: 500, height: 500, crop: "limit" }],
-    };
-  },
+// Set up storage
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "cleanpro",
+  allowedFormats: ["jpg", "jpeg", "png"],
+  transformation: [{ width: 500, height: 500, crop: "limit" }],
 });
 
-// Multer export
+// Export upload middleware
 const uploadProfile = multer({ storage });
 
 module.exports = uploadProfile;
